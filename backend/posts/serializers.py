15083c0373760b178ca68af1b17252af486e6bda
monte <montenegrojds@gmail.com>
2022-01-12 20:07:14 -0500

    adding nest serializer

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(source='author.username', read_only=True)
     class Meta:
         fields = ('id', 'author', 'title', 'body', 'created_at')
         model = Post
+        #depth = 1
+        
+       
+       
