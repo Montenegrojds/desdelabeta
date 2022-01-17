f6098fd212a52f132c51f4143386c64832dca30f
monte <montenegrojds@gmail.com>
2022-01-16 19:08:37 -0500

    navbar

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,11 @@
+
+import {Container, Navbar} from 'react-bootstrap'
+export const NavBarApp  = ()=>{
+    return(
+        <Navbar bg="light" expand="lg">
+            <Container>
+            <Navbar.Brand href="#">DESDE LA BETA</Navbar.Brand>
+            </Container>
+        </Navbar>
+    )
+}
