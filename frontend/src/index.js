725f24b3c13b3a352e6794d196843bbd9ff1747d
monte <montenegrojds@gmail.com>
2022-01-26 08:10:08 -0500

    adding routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import {Header} from "./components/navbar"
+import { FullPost } from './components/fullpost';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { App } from './App';
+
+const routing =(
+  <BrowserRouter>  
+     <React.StrictMode>
+    <Header />
+    <Routes>
+        <Route  path="/" element={ <App /> } />
+        <Route path="/post/:id" element={<FullPost/>} /> 
+    </Routes>
+   
+
+  </React.StrictMode>
+  </BrowserRouter>
+
+  
 );
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
