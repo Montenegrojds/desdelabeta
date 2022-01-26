d473b91ad87ada2844585cbac01f1f599580794f
monte <montenegrojds@gmail.com>
2022-01-26 08:08:57 -0500

    adding navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,10 +1,20 @@
 
-import {Container, Navbar} from 'react-bootstrap'
-export const NavBarApp  = ()=>{
+import {Container, Navbar, NavDropdown, Nav} from 'react-bootstrap'
+export const Header  = ()=>{
     return(
         <Navbar bg="light" expand="lg">
             <Container>
-            <Navbar.Brand href="#">DESDE LA BETA</Navbar.Brand>
+                <Navbar.Brand href='/'>
+                DESDE LA BETA
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="#">Lo ultimo</Nav.Link>
+              
+                </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
